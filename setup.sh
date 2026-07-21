14599ad7b47476ba1d718edd90663cd45400d68b
ndu-ik <8ikxud6nt@proton.me>
2026-07-21 23:28:38 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
-# Clone the repository without any submodules (non-recursive)
-git clone --no-recurse-submodules https://github.com/ndu-ik/ndu-ndi.git
+# 1. Clone without checking out any files or submodules initially
+git clone --no-checkout --no-recurse-submodules https://github.com/ndu-ik/ndu-ndi.git ndu-ndi
 
 # Check if clone was successful
 if [ $? -eq 0 ]; then
     # Change into the directory
     cd ndu-ndi || exit
     
-    # Run the fluidwall.sh script with set-install parameter
+    # 2. Initialize sparse-checkout in cone mode
+    git sparse-checkout init --cone
+    
+    # 3. Explicitly tell git to only check out files at the root level (no subfolders)
+    git sparse-checkout set ""
+    
+    # 4. Checkout the repository with the sparse rules applied
+    git checkout
+     
+    # 5. Run the fluidwall.sh script with set-install parameter
     ./fluidwall.sh set-install
 else
     echo "Failed to clone repository"
